a3da4aeb92128d81ff088922f2129be9086a36c1
B1arni <haraldironbutt@gmail.com>
2022-08-15 00:25:11 +0200

    urls removed

--- urls.js
+++
@@ -1,9 +0,0 @@
-const urls = {
-  cashInConfig: "https://developers.paysera.com/tasks/api/cash-in",
-  cashOutJuridicalConfig:
-    "https://developers.paysera.com/tasks/api/cash-out-juridical",
-  cashOutNaturalConfig:
-    "https://developers.paysera.com/tasks/api/cash-out-natural",
-};
-
-module.exports = urls;
