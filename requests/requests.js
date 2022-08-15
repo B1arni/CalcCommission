03d95b914fbd118ab098fbbe4c98c1078ff74e15
B1arni <haraldironbutt@gmail.com>
2022-08-15 15:17:56 +0200

    Del comments

--- requests/requests.js
+++ requests/requests.js
@@ -1,30 +1,5 @@
 const urls = require("../constants/urls");
 
-// const requestCashInConfig = fetch(urls.cashInConfig)
-//   .then((response) => response.json())
-//   .then((data) => data)
-//   .catch((err) => {
-//     throw err;
-//   });
-// const requestCashOutJuridicalConfig = fetch(urls.cashOutJuridicalConfig)
-//   .then((response) => response.json())
-//   .then((data) => data)
-//   .catch((err) => {
-//     throw err;
-//   });
-// const requestCashOutNaturalConfig = fetch(urls.cashOutNaturalConfig)
-//   .then((response) => response.json())
-//   .then((data) => data)
-//   .catch((err) => {
-//     throw err;
-//   });
-
-// const requestsArr = [
-//   requestCashInConfig,
-//   requestCashOutJuridicalConfig,
-//   requestCashOutNaturalConfig,
-// ];
-
 const requestsArr = urls.map((url) =>
   fetch(url)
     .then((response) => response.json())
